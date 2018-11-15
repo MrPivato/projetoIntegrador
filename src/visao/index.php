cc18fb57c6ac80fa38652a584c786291aa691bd3
João Mário Basso Nunes <39705979+JMBassoN@users.noreply.github.com>
2018-11-15 16:54:50 -0200

    Add files via upload

--- src/visao/index.php
+++ src/visao/index.php
@@ -2,44 +2,66 @@
 include_once 'inc/header.inc.php'
 ?>
 
-    </body>
+</body>
 
+<br/>
+<div class="container">
+    <div class="row justify-content-center ">
+        <h1>Seja bem vindo!</h1>
+    </div>
     <br/>
-    <div class="container">
-        <div class="row justify-content-center align-items-start">
-            <h1 class="col-4">Seja bem vindo!</h1>
-        </div>
-        <br/>
-        <div class="row justify-content-center ">
-            <p class="col-10 phome">lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum lorem ipsumlorem forza ipsumlorem
-                ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum</p>
-        </div>
-        <br clear="all">
-        <div class="row justify-content-center ">
-            <a class="col-4 ahome nav-link" href="cademprestimo.php">Cadastrar empréstimo</a>
-        </div>
-        <br clear="all">
-        <div class="row justify-content-center imghome">
+    
 
-            <div class="col-4">
-                <img class="img-fluid rounded  w-100 m-2 h-75 d-inline-block" src="img/imtemporaria1.jpg" alt="" />
-                <a class="col-md-auto ahome1 nav-link" href="cadlivro">Cadastrar Livros</a>
+
+
+    <div class="row justify-content-center imghome">
+
+        <div id="carouselHome" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselHome" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselHome" data-slide-to="1"></li>
+                <li data-target="#carouselHome" data-slide-to="2"></li>
+                <li data-target="#carouselHome" data-slide-to="3"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/imtemporaria2.jpg" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5><a class="ahome2 nav-link" href="cadLivro.php">Cadastrar Livros</a></h5>
+                        
+                    </div>
+                </div>
+                <div class="carousel-item">
+                        <img src="img/imtemporaria2.jpg" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                        <h5><a class="ahome2 nav-link" href="cadAluno.php">Cadastrar Alunos</a></h5>
+                        
+                        </div>
+                </div>
+                <div class="carousel-item">
+                        <img src="img/imtemporaria2.jpg" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                        <h5><a class="ahome2 nav-link" href="cadCad.php">Cadastrar Categorias</a></h5>
+                        
+                        </div>
+                </div>
+                <div class="carousel-item">
+                        <img src="img/imtemporaria2.jpg" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                        <h5><a class="ahome2 nav-link" href="cadEmprestimo.php">Cadastrar Empréstimos</a></h5>
+                        
+                        </div>
+                </div>
             </div>
-
-            <div class="col-4">
-                <img class="img-fluid rounded  w-100 m-2 h-75 d-inline-block" src="img/imtemporaria2.jpg" alt="" />
-                <a class="col-md-auto ahome1 nav-link" href="cadlivro">Cadastrar Alunos</a>
-            </div>
-
-            <div class="col-4">
-                <img class="img-fluid rounded  w-100 m-2 h-75 d-inline-block" src="img/imtemporaria3.jpg" alt="" />
-                <a class="col-md-auto ahome1 nav-link" href="cadlivro">Cadastrar Categorias</a>
-            </div>
+            <a class="carousel-control-prev" href="#carouselHome" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselHome" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
-
-
-
-
 
 
 
@@ -50,6 +72,17 @@ include_once 'inc/header.inc.php'
 
 
 
-    </html>
+
+
+
+
+
 </div>
+
+
+
+
+</html>
+</div>
+
 </html>
