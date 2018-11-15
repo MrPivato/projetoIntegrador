c70bb9a2ca9033f6644026796dd0171cd5867346
João Mário Basso Nunes <39705979+JMBassoN@users.noreply.github.com>
2018-11-15 16:54:19 -0200

    Add files via upload

--- src/visao/js/app.js
+++ src/visao/js/app.js
@@ -1,1 +1,21 @@
-// keep
+function filtrar() {
+
+            var filtro = $("input[name='filtro']:checked").val();
+
+            var input, filter, table, tr, td, i;
+            input = document.getElementById("inputPesquisa");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("myTable");
+            tr = table.getElementsByTagName("tr");
+
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td")[parseInt(filtro)];//$filtro 
+                if (td) {
+                    if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+        }
