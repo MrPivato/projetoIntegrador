570e7ac873ef689a8eaa2391fc9cfea819251617
João Mário Basso Nunes <39705979+JMBassoN@users.noreply.github.com>
2018-11-17 21:23:48 -0200

    cad e list livro
    creio que não tenha problemas nesse

--- src/visao/cadLivro.php
+++ src/visao/cadLivro.php
@@ -1,34 +1,70 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <link href="css/Cadastro.css" rel="stylesheet" type="text/css"/>   
-		<link href="https://fonts.googleapis.com/css?family=Libre+Franklin" rel="stylesheet">
+   <head>
+      <link href="css/Cadastro.css" rel="stylesheet" type="text/css"/>
+      <link href="https://fonts.googleapis.com/css?family=Libre+Franklin" rel="stylesheet">
+      <?php
+         include_once 'inc/header.inc.php'
+         ?>
+   </head>
+   <?php
+      //Include das classes via autoload
+      include_once '../autoload.php';
+      //Caso tenha sido feito um POST da página
+      if($_POST){
+          //Cria o Controle desta View (página)
+          $livroControle = new ControleLivro();
+      
+          //Passa o POST desta View para o Controle
+          $livroControle->setVisao($_POST);
+          //Verifica qual ação (inserir ou alterar) vai passar para o Controle
+          if(empty($_POST["alterar"])){
+              $retorno = $livroControle->controleAcao("inserir");
+              if($retorno) {$msg="Livro inserido com sucesso!";}
+              else{$erro="Houve um erro na inserção do livro!";}
+          }else{
+              $retorno = $livroControle->controleAcao("alterar");
+              if($retorno) {$msg="Livro alterado com sucesso!";}
+              else{$erro="Houve um erro na alteração do livro!";}
+          }
+         
+      }
+      
+      ?>
+   </head>
     <body id="cadLivroForm">
-		<?php
-			include "inc/header.inc.php";
-		?>
+	<?php 
+                    //Imprime as mensagens
+                    if(isset($msg)){ 
+                        echo "<div class='alert alert-success' id='msg' name='msg'>".$msg."</div>";                             
+                        $msg=null;
+                    }
+                    if(isset($erro)){ 
+                        echo "<div class='alert alert-danger' id='msg' name='erro'>".$erro."</div>"; 
+                        $msg=null;
+                    }                
+               ?>          
 	<form action="cadLivro.php" method="POST" name="cad_livro">
         <div class="container">
 		<br clear="all">
             <div class="main-div">
             <div class="cadLivro-form">
-            <div class="form-group">
+           
 				<h2>Cadastro de Livros</h2>
-			<div class="custom-file">
-				<input type="file" name="custom-file" class="custom-file-input" id="customFile">
-				<label for="imagem" class="custom-file-label" for="customFile">Escolha uma imagem</label>
-			</div>
-			</div>
+			
             <div class="form-group">
 				<label for="categoria">Escolha a categoria</label>
-					<select id="categoria" name="categoria" class="form-control" required="">
-						<option selected disabled selected>Escolha a categoria</option>
-						<option>Biologia</option>
-						<option>Matemática</option>
-						<option>Física</option>
-						<option>Geografia</option>
-						<option>Português</option>
-						<option>Quimica</option>
+					<select id="grande_area" name="grande_area" class="form-control" required="">
+					<?php
+$categoriaControle = new ControleCategoria;
+$listCategoria = new Categoria;
+
+$categorias = array();
+$categorias = $categoriaControle->controleAcao("listarTodos");
+
+$listCategoria->printCategorias($categorias);
+?>
+    
 					</select>
 			</div>	
 			<div class="form-group">
@@ -50,8 +86,33 @@
             </div>
 			<div class="form-group">
 				<label for="ISBN">Informe o ISBN</label>
-                <input  id="ISBN" name="ISBN" type="number" placeholder="Informe o ISBN" class="form-control" required="" style="padding-top:0px;">
-            </div> 
+                <input  id="isbn" name="isbn" type="number" placeholder="Informe o ISBN" class="form-control" required="" style="padding-top:0px;">
+			</div> 
+			<div class="form-group">
+				<label for="quantidade">Quantidade</label>
+                <input  id="quantidade" name="quantidade" type="number" placeholder="Informe a quantidade" class="form-control" required="" style="padding-top:0px;">
+			</div> 
+
+			<div class="form-group">
+				<label for="volume">Condições</label>
+					<select id="condicao" name="condicao" class="form-control" required="">
+						<option selected disabled selected>Escolha o volume</option>
+						<option>Ótimo</option>
+						<option>Regular</option>
+						<option>Levemente estragado</option>
+						<option>Estragado</option>
+					</select>
+			</div>
+
+			<div class="form-group">
+				<label for="volume">Status</label>
+					<select id="status" name="status" class="form-control" required="">
+						<option selected disabled selected>Escolha o volume</option>
+						<option>Pendente</option>
+						<option>Disponível</option>
+					</select>
+			</div>
+
                 <button id="login" name="login" class="btn btn-primary">Confirmar</button>
                         
                 <button id="reset" name="cancelar" type="reset" class="botao-rst" class="btn btn-primary">Cancelar</button>
--- src/visao/listLivro.php
+++ src/visao/listLivro.php
@@ -42,78 +42,20 @@ $livros = $livroControle->controleAcao("listarTodos");
                     <th>Nome</th>
                     <th>Volume</th>
                     <th>Autor</th>
-                    <th>Quantidade em estoque</th>
                     <th>Grande Área</th>
+                    <th>Status</th>
+                    <th>Condição</th>
                     <th>Açao</th>
                 </tr>
             </thead>
             <tbody id="myTable">
                 <tr>
-                    <td>0013432</td>
-                    <td>
-                        <a href="#">Biologia 3</a>
-                    </td>
-                    <td>3</td>
-                    <td>Marcos </td>
-                    <td>10</td>
-                    <td>Biologia</td>
 
-
-                    <td>
-                        <!-- Alterar -->
-                        <button type="button" class="btn btn-warning text-light" data-toggle="modal" data-target="#exampleModalCenter">
-                            <i class="fas fa-edit"></i>
-                        </button>
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <button type="button" class="close ml-auto" data-dismiss="modal" aria-label="Close">X &nbsp; </button>
-                                    <?php 
-		include_once "cadAluno.php";
-	  ?>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Deletar -->
-                        <button type="button" class="btn btn-danger text-light" data-toggle="modal" data-target="#cpp2">
-                            <i class="fas fa-trash-alt"></i>
-                        </button>
-                        <!-- Modal -->
-                        <div class="modal fade" id="cpp2" tabindex="-1" role="dialog" aria-labelledby="cpp2" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <div class='modal-body'>
-                                        <p class='text-dark'>Deseja realmente excluir?</p>
-                                    </div>
-                                    <div class='modal-footer'>
-                                        <a href='listcrianca.php?id={$registro[' id ']}' type='button' class='btn btn-success' id='delete'>Confirmar</a>
-                                        <button type='button' data-dismiss='modal' class='btn btn-danger'>Cancelar</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- -->
-                        <!-- Mostrar todos -->
-                        <button type="button" class="btn btn-info text-light" data-toggle="modal" data-target="#modalExemplares">
-                            <i class="fas fa-clipboard-list"></i>
-                        </button>
-                        <!-- Modal -->
-                        <div class="modal fade bd-example-modal-lg" id="modalExemplares" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
-                            aria-hidden="true">
-                            <div class="modal-dialog modal-lg" role="document">
-                                <div class="modal-content">
-                                    <button type="button" class="close ml-auto" data-dismiss="modal" aria-label="Close">X &nbsp; </button>
-                                    <?php 
-		include_once "listExemplares.php";
-	  ?>
-                    </td>
-                </tr>
 
 <?php 
 $list->printTodos($livros);
 ?>
-
+</tr>
             </tbody>
         </table>
         </div>
