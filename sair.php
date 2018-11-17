38f12fa793694adada96f0eabb9f21636e53f4e9
WillianFranchini <45112218+WillianFranchini@users.noreply.github.com>
2018-11-16 23:15:16 -0200

    Delete sair.php

--- sair.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// Inicia sessões, para assim poder destruí-las 
-session_start(); 
-session_destroy(); 
- 
-setcookie("logado");
-setcookie("usuario");   
-
-header("Location: login.php"); 
