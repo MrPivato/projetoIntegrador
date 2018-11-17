c184c97e9dbe40ef3f1b4e16c6af8ca656c74160
MrPivato <pivatogabriel@gmail.com>
2018-11-17 19:44:31 -0200

    BD novo

--- src/sql/bd.sql
+++ src/sql/bd.sql
@@ -2,74 +2,73 @@ CREATE DATABASE projetoIntegrador;
 USE projetoIntegrador;
 
 CREATE TABLE Funcionario (
-    id INTEGER PRIMARY KEY,
-    nome VARCHAR(100),
-    email VARCHAR(100),
-    senha VARCHAR(100),
-    status VARCHAR(50)
+        id INTEGER PRIMARY KEY,
+        nome VARCHAR(100),
+        email VARCHAR(100),
+        senha VARCHAR(100),
+        status VARCHAR(50)
 );
 
 CREATE TABLE Categoria (
-    categoria VARCHAR(100) PRIMARY KEY
+        categoria VARCHAR(100) PRIMARY KEY
 );
 
 CREATE TABLE Curso (
-    curso VARCHAR(100) PRIMARY KEY
+        curso VARCHAR(100) PRIMARY KEY
 );
 
 CREATE TABLE Estudante (
-    matricula CHAR(12) PRIMARY KEY,
-    nome VARCHAR(100),
-    curso VARCHAR(100),
-    email VARCHAR(100),
-    status VARCHAR(50)
+        matricula CHAR(12) PRIMARY KEY,
+        nome VARCHAR(100),
+        curso VARCHAR(100),
+        email VARCHAR(100),
+        status VARCHAR(50)
 );
 
 CREATE TABLE Livro (
-    isbn VARCHAR(100),
-    nome VARCHAR(100),
-    volume INTEGER,
-    autor VARCHAR(100),
-    codBarras VARCHAR(100) PRIMARY KEY,
-    status VARCHAR(50),
-    condicao VARCHAR(100),
-    grande_area VARCHAR(100)
+        isbn VARCHAR(100),
+        nome VARCHAR(100),
+        volume INTEGER,
+        autor VARCHAR(100),
+        codBarras VARCHAR(100) PRIMARY KEY,
+        status VARCHAR(50),
+        condicao VARCHAR(100),
+        grande_area VARCHAR(100)
 );
 
 CREATE TABLE Emprestimo (
-    matriculaEstudante CHAR(12),
-    codBarrasLivro VARCHAR(100),
-    verificacaoEntrega INTEGER,
-    id INTEGER PRIMARY KEY,
-    dataDevolucao DATE,
-    periodoEntrega DATE,
-    statusEntrega VARCHAR(50),
-    condicaoEntrega VARCHAR(100),
-    condicaoDevolucao VARCHAR(100),
-    dataDeEntrega DATE
+        matriculaEstudante CHAR(12),
+        verificacaoEntrega INTEGER,
+        codBarrasLivro VARCHAR(100) PRIMARY KEY,
+        dataDevolucao DATE,
+        periodoEntrega DATE,
+        statusEntrega VARCHAR(50),
+        condicaoEntrega VARCHAR(100),
+        condicaoDevolucao VARCHAR(100),
+        dataDeEntrega DATE
 );
- 
+
 ALTER TABLE Livro ADD CONSTRAINT FK_Livro_2
-    FOREIGN KEY (grande_area)
-    REFERENCES Categoria (categoria)
-    ON DELETE RESTRICT;
- 
- ALTER TABLE Estudante ADD CONSTRAINT FK_Estudante_2
-    FOREIGN KEY (curso)
-    REFERENCES Curso (curso)
-    ON DELETE RESTRICT;
- 
+FOREIGN KEY (grande_area)
+REFERENCES Categoria (categoria)
+ON DELETE RESTRICT;
+
+ALTER TABLE Estudante ADD CONSTRAINT FK_Estudante_2
+FOREIGN KEY (curso)
+REFERENCES Curso (curso)
+ON DELETE RESTRICT;
+
 ALTER TABLE Emprestimo ADD CONSTRAINT FK_Emprestimo_1
-    FOREIGN KEY (matriculaEstudante)
-    REFERENCES Estudante (matricula)
-    ON DELETE NO ACTION;
- 
+FOREIGN KEY (matriculaEstudante)
+REFERENCES Estudante (matricula)
+ON DELETE NO ACTION;
+
 ALTER TABLE Emprestimo ADD CONSTRAINT FK_Emprestimo_2
-    FOREIGN KEY (codBarrasLivro)
-    REFERENCES Livro (codBarras)
-    ON DELETE NO ACTION;
- 
+FOREIGN KEY (codBarrasLivro)
+REFERENCES Livro (codBarras)
+ON DELETE NO ACTION;
+
 ALTER TABLE Emprestimo ADD CONSTRAINT FK_Emprestimo_3
-    FOREIGN KEY (verificacaoEntrega)
-    REFERENCES Funcionario (id)
-    ON DELETE NO ACTION;
+FOREIGN KEY (verificacaoEntrega)
+REFERENCES Funcionario (id)
+ON DELETE NO ACTION;
