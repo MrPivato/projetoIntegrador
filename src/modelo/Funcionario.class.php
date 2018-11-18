b2fc16b225853ba8742fe66996bd85dcc78151a1
João Mário Basso Nunes <39705979+JMBassoN@users.noreply.github.com>
2018-11-18 18:12:00 -0200

    Livro e Funcionário modelo
    arrumei o livro e fiz o func

--- src/modelo/Funcionario.class.php
+++ src/modelo/Funcionario.class.php
@@ -5,9 +5,8 @@ class Funcionario implements IBaseModelo{
         // vars -------------------------------------
         private $id;
         private $nome;
-        private $curso;
-        private $turma;
         private $email;
+        private $senha;
         private $status;
 
         private $conn;
@@ -22,17 +21,14 @@ class Funcionario implements IBaseModelo{
                 return $this->nome;
         }
 
-        public function getCurso() {
-                return $this->curso;
-        }
-
-        public function getTurma() {
-                return $this->turma;
-        }
-
         public function getEmail() {
                 return $this->email;
         }
+
+        public function getSenha() {
+                return $this->senha;
+        }
+
 
         public function getStatus() {
                 return $this->status;
@@ -47,16 +43,12 @@ class Funcionario implements IBaseModelo{
                 $this->nome = $nome;
         }
 
-        public function setCurso($curso) {
-                $this->curso = $curso;
-        }
-
-        public function setTurma($turma) {
-                $this->turma = $turma;
-        }
-
         public function setEmail($email) {
                 $this->email = $email;
+        }
+
+        public function setSenha($senha) {
+                $this->senha = $senha;
         }
 
         public function setStatus($status) {
@@ -78,16 +70,15 @@ class Funcionario implements IBaseModelo{
                 try{
                         //Comando SQL para inserir um funcionario
                         $query="INSERT INTO Funcionario 
-                                VALUES (:id, :nome, :curso, :turma, :email, :status) ";
+                                VALUES (:id, :nome, :email, :senha, :status) ";
 
                         $this->stmt= $this->conn->prepare($query);
 
-                        $this->stmt->bindValue(':id', $this->nome, PDO::PARAM_INT);
+                        $this->stmt->bindValue(':id', $this->id, PDO::PARAM_STR);
                         $this->stmt->bindValue(':nome', $this->nome, PDO::PARAM_STR);
-                        $this->stmt->bindValue(':curso', $this->curso, PDO::PARAM_STR);
-                        $this->stmt->bindValue(':turma', $this->nome, PDO::PARAM_INT);
                         $this->stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
-                        $this->stmt->bindValue(':status', $this->nome, PDO::PARAM_INT);
+                        $this->stmt->bindValue(':senha', $this->senha, PDO::PARAM_INT);
+                        $this->stmt->bindValue(':status', $this->status, PDO::PARAM_INT);
 
                         if($this->stmt->execute()){
                                 return true;
@@ -104,19 +95,16 @@ class Funcionario implements IBaseModelo{
                         //Comando SQL para inserir um funcionario
                         $query="UPDATE Funcionario 
                                 SET nome = :nome, 
-                                curso = :curso, 
-                                turma = :turma, 
+                                senha = :senha, 
                                 email = :email, 
                                 status = :status 
                                 WHERE id=:id ";
                         $this->stmt= $this->conn->prepare($query);
 
-                        $this->stmt->bindValue(':id', $this->nome, PDO::PARAM_INT);
                         $this->stmt->bindValue(':nome', $this->nome, PDO::PARAM_STR);
-                        $this->stmt->bindValue(':curso', $this->curso, PDO::PARAM_STR);
-                        $this->stmt->bindValue(':turma', $this->nome, PDO::PARAM_INT);
+                        $this->stmt->bindValue(':senha', $this->senha, PDO::PARAM_INT);
                         $this->stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
-                        $this->stmt->bindValue(':status', $this->nome, PDO::PARAM_INT);
+                        $this->stmt->bindValue(':status', $this->status, PDO::PARAM_INT);
 
 
                         if($this->stmt->execute()){
@@ -152,13 +140,13 @@ class Funcionario implements IBaseModelo{
                         //Comando SQL para inserir um funcionario
                         if(!is_null($nome)){
                                 //Pesquisa pelo nome
-                                $query="SELECT id,nome,curso,turma,email,status FROM Funcionario WHERE nome LIKE :nome";
+                                $query="SELECT id,nome,senha,email,status FROM Funcionario WHERE id LIKE :id";
                         }else{
                                 // Pesquisa todos
-                                $query="SELECT id,nome,curso,turma,email,status FROM Funcionario";
+                                $query="SELECT id,nome,senha,email,status  FROM Funcionario";
                         }
                         $this->stmt= $this->conn->prepare($query);
-                        if(!is_null($nome))$this->stmt->bindValue(':nome', '%'.$nome.'%', PDO::PARAM_STR);
+                        if(!is_null($nome))$this->stmt->bindValue(':id', '%'.$nome.'%', PDO::PARAM_STR);
 
                         if($this->stmt->execute()){
                                 // Associa cada registro a uma classe Funcionario
@@ -178,7 +166,7 @@ class Funcionario implements IBaseModelo{
         public function listarUnico($id){
 
                 try{
-                        $query="SELECT id,nome,curso,turma,email,status FROM Funcionario WHERE id=:id";
+                        $query="SELECT id,nome,senha,email,status FROM Funcionario WHERE id=:id";
                         $this->stmt= $this->conn->prepare($query);
                         $this->stmt->bindValue(':id', $id, PDO::PARAM_INT);
 
@@ -194,5 +182,53 @@ class Funcionario implements IBaseModelo{
                         return null;
                 }
 
+        }
+
+
+        public function printTodos($categorias)
+        {
+                if(!empty($categorias)){
+                        foreach ($categorias as $est) {
+                                echo "<tr>
+                                        <td>".$est->getId()."</td>
+                                        <td>".$est->getNome()."</td>
+                                        <td>".$est->getEmail()."</td>
+                                        <td>".$est->getSenha()."</td>
+                                        <td>".$est->getStatus()."</td>
+                                        " ;  
+                                echo '
+                              <td>
+                            <!-- Alterar -->
+                            <a href="../visao/cadFuncionario.php?matricula='.$est->getId().'&op=alt"><button type="button" class="btn btn-warning text-light" data-toggle="modal" data-target="#exampleModalCenter">
+                            <i class="fas fa-edit"></i>
+                            </button></a>
+                            <!-- Deletar -->
+                            <button type="button" class="btn btn-danger text-light" data-toggle="modal" data-target="#cpp2">
+                            <i class="fas fa-trash-alt"></i>
+                            </button>
+                            <!-- Modal -->
+                            <div class="modal fade" id="cpp2" tabindex="-1" role="dialog" aria-labelledby="cpp2" aria-hidden="true">
+                               <div class="modal-dialog modal-dialog-centered" role="document">
+                                  <div class="modal-content">
+                                     <div class=\'modal-body\'>
+                                        <p class=\'text-dark\'>Deseja realmente excluir?</p>
+                                     </div>
+                                     <div class=\'modal-footer\'>
+                                        <a href=\'listcrianca.php?id={$registro[\' id \']}\' type=\'button\' class=\'btn btn-success\' id=\'delete\'>Confirmar</a>
+                                        <button type=\'button\' data-dismiss=\'modal\' class=\'btn btn-danger\'>Cancelar</button>
+                                     </div>
+                                  </div>
+                               </div>
+                            </div>
+                            <!-- Mostrar todos -->
+                            <a href=\'listEmprestimos.php?id={$registro[\' id \']}\' class="btn btn-info text-light">
+                            <i class="fas fa-clipboard-list"></i>
+                            </a>
+                            <!-- -->
+                         </td>
+                       </tr>
+                                  ';
+                        }
+                }
         }
 }
