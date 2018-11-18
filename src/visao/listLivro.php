5c5b9c9c6c65732041cf0b06543bf56b0757813b
WillianFranchini <45112218+WillianFranchini@users.noreply.github.com>
2018-11-18 12:21:48 -0200

    redirecionamento.inc.php

--- src/visao/listLivro.php
+++ src/visao/listLivro.php
@@ -1,16 +1,6 @@
-<?php
-    session_start();
-    if (isset($_COOKIE["logado"])) {
-        if ($_COOKIE["logado"] == 'on') {
-            $_SESSION["usuario"] = $_COOKIE["usuario"];
-        }
-    }
-    if (!isset($_SESSION["usuario"])) {
-        header("Location: login.php");
-        exit;
-    }
-    $usuario = $_SESSION['usuario'];
-	
+
+<?php 
+	include_once 'inc/redirecionamento.inc.php';
 ?>
 
 
