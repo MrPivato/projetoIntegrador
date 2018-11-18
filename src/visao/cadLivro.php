bbf29fe872e83bf1b65a69946f99f5af768622ca
WillianFranchini <45112218+WillianFranchini@users.noreply.github.com>
2018-11-18 11:56:28 -0200

    add redirecionamento

--- src/visao/cadLivro.php
+++ src/visao/cadLivro.php
@@ -1,3 +1,20 @@
+<?php
+    session_start();
+    if (isset($_COOKIE["logado"])) {
+        if ($_COOKIE["logado"] == 'on') {
+            $_SESSION["usuario"] = $_COOKIE["usuario"];
+        }
+    }
+    if (!isset($_SESSION["usuario"])) {
+        header("Location: login.php");
+        exit;
+    }
+    $usuario = $_SESSION['usuario'];
+	
+?>
+
+
+
 <!DOCTYPE html>
 <html>
    <head>
