e74a4b51de838e01068c05d9b1f94137412e5de1
WillianFranchini <45112218+WillianFranchini@users.noreply.github.com>
2018-11-18 11:57:06 -0200

    add redirecionamento

--- src/visao/detalheEmprestimo.php
+++ src/visao/detalheEmprestimo.php
@@ -1,4 +1,22 @@
-    <?php
+  <?php
+    session_start();
+    if (isset($_COOKIE["logado"])) {
+        if ($_COOKIE["logado"] == 'on') {
+            $_SESSION["usuario"] = $_COOKIE["usuario"];
+        }
+    }
+    if (!isset($_SESSION["usuario"])) {
+        header("Location: login.php");
+        exit;
+    }
+    $usuario = $_SESSION['usuario'];
+	
+?>  
+
+
+
+
+<?php
      include_once 'inc/header.inc.php'
      ?>
 
