6fd3e637426e7da70391e015ea7e4ef182e4d8ce
João Mário Basso Nunes <39705979+JMBassoN@users.noreply.github.com>
2018-11-18 23:52:45 -0200

    listEmprestimo

--- src/visao/listEmprestimos.php
+++ src/visao/listEmprestimos.php
@@ -1,13 +1,19 @@
-
 <?php 
 	include_once 'inc/redirecionamento.inc.php';
-?>
+
+//Include das classes via autoload
+include_once '../autoload.php';
+//Cria o Controle desta View e instaância o Estudante
+$emprestimoControle = new ControleEmprestimo();
+$list = new Emprestimo;
+
+$emprestimos = array();
+$emprestimos = $emprestimoControle->controleAcao("listarTodos");
+// Vamos fazer um include só com o form, que passa informaçoes para
+// outra página, e está faz as alterações
+// daí redireciona pra esta aqui
 
 
-
-
-
-<?php
     include_once 'inc/header.inc.php'
     ?>
 <body>
@@ -37,115 +43,10 @@
             </thead>
             <tbody id="myTable">
                 <tr>
-                    <td>INFO</td>
-                    <td>
-                        <a href="listAluno.php?{$registro['id']}">Jose corno viado da silva</a>
-                        <hr>
-                        <b>201710040123</b>
-                    </td>
-                    <td>
-                        <div class="container">
-                            <p>
-                                <button class="btn btn-info bot-list" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false"
-                                    aria-controls="collapseExample">
-                                Mostrar todas informações do livro
-                                </button>
-                            </p>
-                            <div class="collapse" id="collapseExample">
-                                <ul class="list-group">
-                                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                                        <a href="#">MAT1-030</a> 
-                                    </li>
-                                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                                        <a href="#">Funcionário que verificou</a> 
-                                        <span class="badge badge-info badge-pill">Verificador</span>
-                                        <!-- fica aqui para ser preenchido -->
-                                    </li>
-                                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                                        Condição de recebimento
-                                        <span class="badge badge-success badge-pill">Novo</span>
-                                    </li>
-                                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                                        Condição de entrega
-                                        <span class="badge badge-warning badge-pill">Usado</span>
-                                    </li>
-                                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                                        Status entrega
-                                        <span class="badge badge-warning badge-pill">Pendente</span>
-                                    </li>
-                                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                                        Data de recebimento
-                                        <span class="badge badge-primary badge-pill">20/01/2018</span>
-                                    </li>
-                                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                                        Data de devolução
-                                        <span class="badge badge-secondary badge-pill">20/01/2018</span>
-                                    </li>
-                                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                                        Prazo para entrega
-                                        <span class="badge badge-danger badge-pill">20/12/2018</span>
-                                    </li>
-                                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                                        Periodo de entrega
-                                        <span class="badge badge-dark badge-pill">01/01/2018 - 20/12/2018</span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </td>
-                    <td>
-                        <!-- Alterar -->
-                        <button type="button" class="btn btn-warning text-light" data-toggle="modal" data-target="#cpp"> 
-                        <i class="fas fa-edit"></i>
-                        </button>
-                        <!-- Modal -->
-                        <div class="modal fade" id="cpp" tabindex="-1" role="dialog" aria-labelledby="cpp" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <button type="button" class="close ml-auto" data-dismiss="modal" aria-label="Close">X &nbsp; </button>
-                                    <?php 
-                                        include_once "cadAluno.php";
-                                        ?>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- -->
-                        <!-- Deletar -->
-                        <button type="button" class="btn btn-danger text-light" data-toggle="modal" data-target="#cpp2"> 
-                        <i class="fas fa-trash-alt"></i>
-                        </button>
-                        <!-- Modal -->
-                        <div class="modal fade" id="cpp2" tabindex="-1" role="dialog" aria-labelledby="cpp2" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <div class='modal-body'>
-                                        <p class='text-dark'>Deseja realmente excluir?</p>
-                                    </div>
-                                    <div class='modal-footer'>
-                                        <a href='listcrianca.php?id={$registro['id']}' type='button' class='btn btn-success' id='delete'>Confirmar</a>
-                                        <button type='button' data-dismiss='modal' class='btn btn-danger'>Cancelar</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- -->
-                        <!-- Mostrar todos -->
-                        <button type="button" class="btn btn-info text-light" data-toggle="modal" data-target="#cpp3"> 
-                        <i class="fas fa-clipboard-list"></i>
-                        </button>
-                        <!-- Modal -->
-                        <div class="modal fade" id="cpp3" tabindex="-1" role="dialog" aria-labelledby="cpp3" aria-hidden="true">
-                            <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <button type="button" class="close ml-auto" data-dismiss="modal" aria-label="Close">X &nbsp; </button>
-                                    <?php 
-                                        include_once "listExemplares.php";
-                                    ?>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- -->
-                    </td>
+
+                    <?php 
+$list->printTodos($emprestimos);
+?>
                 </tr>
             </tbody>
         </table>
